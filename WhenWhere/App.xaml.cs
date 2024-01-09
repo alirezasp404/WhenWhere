7e96d2cd42afd5569d9bc058f6339c34d1ef1b8e
AlirezaSadeghiPour <alirezasp404@gmail.com>
2024-01-09 18:51:27 +0330

    initial commit

---
+++ WhenWhere/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WhenWhere
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
