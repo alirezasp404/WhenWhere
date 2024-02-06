4a38a8f6eb0dbed7fdfa9dc64070141ee4581bc3
AlirezaSadeghiPour <alirezasp404@gmail.com>
2024-02-06 19:15:30 +0330

    implementing Home.xaml

---
+++ WhenWhere/Models/EventModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WhenWhere.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int Capacity {  get; set; }
+
+        public string? Location {  get; set; }
+
+        public string? Creator {  get; set; }
+    }
+}
--- WhenWhere/Pages/Home.xaml.cs
+++ WhenWhere/Pages/Home.xaml.cs
@@ -1,9 +1,86 @@
+using System.Collections.ObjectModel;
+using WhenWhere.Models;
+
 namespace WhenWhere.Pages;
 
 public partial class Home : ContentPage
 {
+	public ObservableCollection<EventModel> Events { get; set; }=new ObservableCollection<EventModel>();
 	public Home()
 	{
-		InitializeComponent();
-	}
+        Preferences.Get("UserID", null);
+        InitializeComponent();
+        BindingContext = this;
+		var event1=new EventModel()
+		{
+			Title="eve1",
+			Description= "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
+			Id=1,
+			Location="wwoegjowjeg",
+			DateTime=DateTime.Now,
+			Capacity=10,
+			Creator="owegjo"
+		};
+        var event2 = new EventModel()
+        {
+            Title = "eve2",
+            Description = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
+            Id = 1,
+            Location = "wwoegjowjeg",
+            DateTime = DateTime.Now,
+            Capacity = 10,
+            Creator = "owegjo"
+        };
+        var event3 = new EventModel()
+        {
+            Title = "eve3",
+            Description = "owjgojw",
+            Id = 1,
+            Location = "wwoegjowjeg",
+            DateTime = DateTime.Now,
+            Capacity = 10,
+            Creator = "owegjo"
+        }; var event4 = new EventModel()
+        {
+            Title = "eve1",
+            Description = "owjgojw",
+            Id = 1,
+            Location = "wwoegjowjeg",
+            DateTime = DateTime.Now,
+            Capacity = 10,
+            Creator = "owegjo"
+        };
+        var event5 = new EventModel()
+        {
+            Title = "eve2",
+            Description = "owjgojw",
+            Id = 1,
+            Location = "wwoegjowjeg",
+            DateTime = DateTime.Now,
+            Capacity = 10,
+            Creator = "owegjo"
+        };
+        var event6 = new EventModel()
+        {
+            Title = "eve3",
+            Description = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
+            Id = 1,
+            Location = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
+            DateTime = DateTime.Now,
+            Capacity = 10,
+            Creator = "owegjo"
+        };
+        Events.Add(event1); 
+        Events.Add(event2);
+        Events.Add(event3);
+        Events.Add(event4);
+        Events.Add(event5);
+        Events.Add(event6);
+        
+    }
+
+    private void RegisterButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
