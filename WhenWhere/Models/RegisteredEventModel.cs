d5499427a2fe1bdb35bf61c264017dd77431ae94
AlirezaSadeghiPour <alirezasp404@gmail.com>
2024-02-08 14:50:02 +0330

    add RegisteredEventModel and icons

---
+++ WhenWhere/Models/RegisteredEventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenWhere.Models
+{
+    public class RegisteredEventModel
+    {
+        public int id { get; set; }
+        public string? event_picker { get; set; }
+        public List<int>? events { get; set; }
+
+    }
+}
