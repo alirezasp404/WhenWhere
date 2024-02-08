ee5eeec46ad7da98a3aa9c84c83e3e457fc697e2
Ehsan <ehsanmelm82@gmail.com>
2024-02-08 14:23:18 +0330

    feat : create cs file for "create event "

--- WhenWhere/Pages/CreatedEvents.xaml.cs
+++ WhenWhere/Pages/CreatedEvents.xaml.cs
@@ -1,9 +1,42 @@
+using System.Collections.ObjectModel;
+using WhenWhere.Models;
+using WhenWhere.Services;
+
 namespace WhenWhere.Pages;
 
 public partial class CreatedEvents : ContentPage
 {
-	public CreatedEvents()
-	{
-		InitializeComponent();
-	}
+    private string? userId;
+    public ObservableCollection<EventModel> AllEvents { get; set; } = new ObservableCollection<EventModel>();
+    public CreatedEvents()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        userId = Preferences.Get("UserID", null);
+        if (userId == null)
+            await Shell.Current.GoToAsync("Landing");
+
+        try
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                throw new Exception("It seems that your internet connection has been lost. Please check your connection and try again.");
+            var events = await EventsService.GetAllEvents($"create_event/{userId}/");
+            foreach (var eventModel in events)
+            {
+                if (!AllEvents.Contains(eventModel))
+                {
+                    AllEvents.Add(eventModel);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Failed", $"{ex.Message}", "OK");
+        }
+
+    }
 }
